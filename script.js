3beeb5e8edf8e235a986a97d48cc7982c2a40f51
srilokanas <67470659+srilokanas@users.noreply.github.com>
2022-03-04 03:35:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function getAge(dateString) 
+{
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    return age;
+}
+
+
+let age = getAge('1996-05-05');
+document.getElementById('age').innerHTML = age ;
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
