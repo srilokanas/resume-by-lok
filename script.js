3c2d9a28fd161414623a02d0437aff277da0814b
srilokanas <67470659+srilokanas@users.noreply.github.com>
2022-03-04 14:56:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,22 +17,13 @@ document.getElementById('age').innerHTML = age ;
 
 window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myNavbar");
-var sticky = header.offsetTop;
+var navbar = document.getElementById("myNavbar");
+var sticky = navbar.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
   } else {
-    header.classList.remove("sticky");
-  }
-}
-
-function myNavbar() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
+    navbar.classList.remove("sticky");
   }
 }
